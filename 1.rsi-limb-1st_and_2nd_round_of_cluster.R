994d9807cbf85fe6d7826a1831bba11ddf85964f
bettycatherine <bettycatherine@126.com>
2024-08-19 17:30:25 +0800

    Add files via upload

--- 1.rsi-limb-1st_and_2nd_round_of_cluster.R
+++ 1.rsi-limb-1st_and_2nd_round_of_cluster.R
@@ -7,7 +7,6 @@ library(Rtsne)
 library(sctransform)
 library(ggplot2)
 
-setwd("D:/whatever/working/project/analysis/rsi-limb/sc")
 options(future.globals.maxSize=7000*1024^2)
 hex.data<-read.table("rsi-hex-rsi_3_rescued_stages.txt", header = T, row.names = 1)
 dt.data<-read.table("rsi-dt-rsi_3_rescued_stages.txt", header = T, row.names = 1)
@@ -63,6 +62,8 @@ names(bbb) <- colnames(rsi.combined)
 table(bbb)
 rsi.combined <- AddMetaData(object = rsi.combined, metadata = bbb, col.name = 'stage.2')
 
+##remove useless cells
+
 Idents(rsi.combined)<-"stage"
 rsi.no15<-subset(rsi.combined, idents = c("CS16","CS18","CS20"))
 
@@ -75,6 +76,17 @@ DefaultAssay(rsi.no15)<-"integrated"
 rsi.no15 <- FindClusters(object = rsi.no15, verbose = FALSE, resolution = 0.05)
 DimPlot(rsi.no15)
 
+DefaultAssay(rsi.no15) <- "RNA"
+rsi.no15 <- FindVariableFeatures(rsi.no15, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(rsi.no15)
+rsi.no15 <- ScaleData(rsi.no15, features = all.genes)
+rsi.no15.markers <- FindAllMarkers(rsi.no15, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+rsi.no15.markers %>%
+  group_by(cluster) %>%
+  top_n(n = 5, wt = avg_log2FC) -> top5
+DoHeatmap(rsi.no15, features = top5$gene)+ scale_fill_gradientn(colors = c("green", "white", "red"))
+DotPlot(rsi.no15, features = unique(top5$gene), dot.scale = 8,cols = "RdYlBu") + RotatedAxis()
+
 cell.type <- c("MP", "chondrocyte","epithelia","muscle","endothelia","neuron","WBC")
 names(cell.type) <- levels(rsi.no15)
 rsi.no15 <- RenameIdents(rsi.no15, cell.type)
@@ -84,49 +96,45 @@ names(cell.type) <- colnames(rsi.no15)
 rsi.no15 <- AddMetaData(object = rsi.no15, metadata = cell.type, col.name = 'cell.type')
 DimPlot(rsi.no15)
 
-sub_cluster <- c("MP_2","MP_10","MP_13", "MP_8","MP_9","MP_3","MP_1","epithelia", 
-                 "MP_12", "chondrocyte",
-"neuron","muscle", "MP_6","MP_5","MP_4","endothelia","MP_7","MP_0","chondrocyte", 
-"chondrocyte","MP_11","WBC","chondrocyte")
-names(sub_cluster) <- levels(x)
-x <- RenameIdents(x, sub_cluster)
-#DimPlot(f, reduction = "tsne", label = TRUE, pt.size = 0.5) + NoLegend()
-sub_cluster<-x@active.ident
-names(sub_cluster) <- colnames(x)
-x <- AddMetaData(object = x, metadata = sub_cluster, col.name = 'sub_cluster')
+##2nd round
+x <- rsi.no15
+Idents(x)<-x$cell.type
+c<-subset(x, idents = "MP")
+DefaultAssay(c)<-"integrated"
+c <- RunPCA(object = c, verbose = FALSE)
+c <- FindNeighbors(object = c, dims = 1:30, verbose = FALSE)
+c <- RunUMAP(object = c, dims = 1:30, verbose = FALSE)
+#c <- RunTSNE(c, dims = 1:20, tsne.method = "FIt-SNE", nthreads = 4, max_iter = 2000)
+c <- FindClusters(object = c, verbose = FALSE, resolution = 0.5)
+
+DefaultAssay(c)<-"RNA"
+c <- NormalizeData(c)
+c <- FindVariableFeatures(c, selection.method = "vst", nfeatures = 2000)
+all.genes <- rownames(c)
+c <- ScaleData(c, features = all.genes)
+markers <- FindAllMarkers(x, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+top5 <- markers %>%group_by(cluster) %>%top_n(n = 5, wt = avg_log2FC)
+DoHeatmap(x, features = top5$gene) + NoLegend()
+DotPlot(x, features = unique(top5$gene),dot.scale = 8,cols = "RdYlBu")+  RotatedAxis()
+
+x$sub_cluster <- as.character(Idents(x))
+x$sub_cluster[Cells(MP)] <- paste0("MP_",(Idents(MP))
+DimPlot(x, group.by = "sub_cluster")
 DimPlot(x)
 
-DefaultAssay(rsi.no15)<-"RNA"
-rsi.no15 <- NormalizeData(rsi.no15)
-rsi.no15 <- FindVariableFeatures(rsi.no15, selection.method = "vst", nfeatures = 2000)
-all.genes <- rownames(rsi.no15)
-rsi.no15 <- ScaleData(rsi.no15, features = all.genes)
-
-markers.to.plot <- c("EBF3", "COL3A1", "DKK2", "ATRNL1", "COL12A1", "GPC5","COL2A1", "COL11A1", "COL9A1", 
-                     "CXCL14", "DSP", "COL17A1", "G2E3", "DLGAP5", "NDC80","LDB2", "ARHGAP26", "EMCN",
-                     "SNTB1", "ADAMTS20", "CDH6","TTN", "NEB", "ACTC1",      
-                     "DAB2", "RBPJ", "MRC1")
-DotPlot(rsi.no15, features = markers.to.plot, dot.scale = 8,cols = "RdYlBu") + RotatedAxis()
-
+Idents(x) <- x$sub_cluster
 markers <- FindAllMarkers(h, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-write.table(markers, "rsi_3-split-h-markers.txt")
 top10 <- markers %>%group_by(cluster) %>%top_n(n = 10, wt = avg_log2FC)
-DoHeatmap(rsi.no15, features = top10$gene) + NoLegend()
-DotPlot(rsi.no15, features = unique(top10$gene))+  RotatedAxis()
+DoHeatmap(x, features = top10$gene) + NoLegend()
+DotPlot(x, features = unique(top10$gene))+  RotatedAxis()
 
 saveRDS(x, "rsi_3-split-250.rds")
-
-#all
-markers.to.plot <- c("COL3A1", "DKK2", "ATRNL1", "COL2A1", "COL11A1", "COL9A1", 
-                     "CXCL14", "DSP", "GRHL2", "TTN", "NEB", "ACTC1", "LDB2", 
-                     "FLT1", "EMCN", "SNTB1", "ADAMTS20", "CDH6", "DAB2", "RBPJ", "MRC1")
-DotPlot(x, features = markers.to.plot, dot.scale = 8,cols = "RdYlBu") + RotatedAxis()
-
 
 levels(x)<-c("MP_0", "MP_1", "MP_2", "MP_3", "MP_4", "MP_5",  "MP_6", "MP_7", 
              "MP_8", "MP_9", "MP_10",  "MP_11", "MP_12", "MP_13", "chondrocyte",
              "epithelia","muscle", "endothelia","neuron", "WBC")
-#MP
+			 
+#find MP markers
 markers.to.plot <- c("PNISR", "AKAP9", "USP47",
                      "GNAS", "ZNF804A", "SASH1",
                      "EBF2",  "EPHA4", "HPSE2",
@@ -146,27 +154,8 @@ markers.to.plot <- c("PNISR", "AKAP9", "USP47",
                      "FLT1", "EMCN", "SNTB1", "ADAMTS20", "CDH6", "DAB2", "RBPJ", "MRC1")
 DotPlot(x, features = markers.to.plot, dot.scale = 8,cols = "RdYlBu") + RotatedAxis()
 
+##annotate MP according to markers
 ##Acording to correlation results combining MPs
-
-Idents(x)<-x$cell.type3
-c<-subset(x, idents = "MP")
-DefaultAssay(c)<-"integrated"
-c <- RunPCA(object = c, verbose = FALSE)
-c <- FindNeighbors(object = c, dims = 1:30, verbose = FALSE)
-c <- RunUMAP(object = c, dims = 1:30, verbose = FALSE)
-#c <- RunTSNE(c, dims = 1:20, tsne.method = "FIt-SNE", nthreads = 4, max_iter = 2000)
-c <- FindClusters(object = c, verbose = FALSE, resolution = 0.5)
-
-DefaultAssay(c)<-"RNA"
-c <- NormalizeData(c)
-c <- FindVariableFeatures(c, selection.method = "vst", nfeatures = 2000)
-all.genes <- rownames(c)
-c <- ScaleData(c, features = all.genes)
-markers <- FindAllMarkers(x, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
-top5 <- markers %>%group_by(cluster) %>%top_n(n = 5, wt = avg_log2FC)
-DoHeatmap(x, features = top5$gene) + NoLegend()
-DotPlot(x, features = unique(top5$gene),dot.scale = 8,cols = "RdYlBu")+  RotatedAxis()
-
 cell.type2 <- c("cop","osteoblast","MP_12", "MP_8","cop","MP_3","MP_0","epithelia", 
                 "MP_12", "chondrocyte",
                 "neuron","muscle", "pe","MP_5","cop","endothelia","cp",
@@ -182,14 +171,7 @@ DimPlot(x)
 levels(x)<-c("MP_0", "cop","MP_3", "MP_5",  "MP_8", "MP_12", "cp","chondrocyte",
              "op",  "osteoblast", "pe", "epithelia", "muscle", "endothelia", "neuron", "WBC" )
 
-x@reductions$umap1<-x@reductions$umap
-x <- RunUMAP(object = x, dims = 1:30, verbose = FALSE)
-x@reductions$umap2<-x@reductions$umap
-x@reductions$tsne1<-x@reductions$tsne
-x <- RunTSNE(x, dims = 1:30, tsne.method = "FIt-SNE", nthreads = 4, max_iter = 2000)
-
 DimPlot(x)
-#figure size:6*7
 
 x<-RenameIdents(x, 'cop'='EMP','osteoblast'='Osteoblast',  "CTP_12"='ZMP', "CTP_8"="MMP",
                 "CTP_3"="AMP", "CTP_0"='PNMP', 'epithelia'= "Epithelia", "chondrocyte"='Chondrocyte',
@@ -198,12 +180,6 @@ x<-RenameIdents(x, 'cop'='EMP','osteoblast'='Osteoblast',  "CTP_12"='ZMP', "CTP_
 levels(x)<-c("PNMP", "EMP", "AMP", "PDMP", "MMP", "ZMP", "CP", "Chondrocyte", "OP", "Osteoblast",
              "PE", "Epithelia", "Muscle", "Endothelia", "Neuron", "BM")
 
-write.csv(FindAllMarkers(x, only.pos = TRUE, min.pct = 0.25, 
-                         logfc.threshold = 0.25),"rsi_3-markers-combined-2.csv")
-
-levels(x)<-c("BM", "Neuron", "Endothelia","Muscle","Epithelia","PE","Osteoblast", "OP", 
-              "Chondrocyte",  "CP","ZMP", "MMP",  
-             "PDMP", "AMP", "EMP", "PNMP"  )
 markers.to.plot <- c("PNISR", "AKAP9", "USP47",
   "EBF2","HPSE2","EPHA4",
   "ARHGAP24", "PLXDC2", "ISM1",
@@ -222,10 +198,7 @@ markers.to.plot <- c("PNISR", "AKAP9", "USP47",
   "DAB2", "RBPJ", "MRC1")
 DotPlot(x, features = markers.to.plot, dot.scale = 8,cols = "RdYlBu") + RotatedAxis()
 
-DotPlot(x, features = markers.to.plot) + 
-  scale_colour_gradient2(low = "navy", high = "darkred", na.value = NA)+
-  geom_point(aes(size=pct.exp), shape = 21, colour="black", stroke=0.5) +
-  guides(size=guide_legend(override.aes=list(shape=21, colour="black", fill="white")))
+
 
 
 
